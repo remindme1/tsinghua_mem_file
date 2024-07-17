09550e097915e36738ae5c990bdea300a746f533
刘源 <liuyuan.renard@bytedance.com>
2024-07-18 01:30:23 +0800

    11

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 set -ex
 cd `dirname $0`
-
+go env -w GOPROXY=https://goproxy.cn,direct
 go build -v -ldflags '-w' -o main
 mkdir -p output/
 mv main output/
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"github.com/cloudwego/hertz/pkg/network/standard"
 
 	"github.com/cloudwego/hertz/pkg/app"
 	"github.com/cloudwego/hertz/pkg/app/server"
@@ -11,7 +12,11 @@ import (
 func main() {
 	// server.Default() creates a Hertz with recovery middleware.
 	// If you need a pure hertz, you can use server.New()
-	h := server.Default()
+	h := server.Default(
+		server.WithHostPorts("127.0.0.1:80"),
+		server.WithMaxRequestBodySize(20<<20),
+		server.WithTransport(standard.NewTransporter),
+	)
 
 	h.GET("/hello", func(ctx context.Context, c *app.RequestContext) {
 		c.String(consts.StatusOK, "Hello hertz!")
